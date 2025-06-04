81f478f1431961cc38ee1cfd1615eb0536ba8d29
Mark Wynn-Mackenzie <mwynnmackenzie@cloudbees.com>
2025-06-04 12:03:55 +0100

    Remove feature flag: fontColor

--- src/feature-management/flags.ts
+++ src/feature-management/flags.ts
@@ -21,8 +21,6 @@ export const flags = {
   showMessage: new Flag(),
   // String - the message to show.
   message: new RoxString('This is the default message; try changing some flag values!'),
-  // String (with options) - the color of the message text.
-  fontColor: new RoxString('Black', ['Red', 'Green', 'Blue', 'Black']),
   // Number (with options) - the size of the message text.
   fontSize: new RoxNumber(12, [12, 16, 24]),
 }
